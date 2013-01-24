c577e8c8733bc57e2fe6c88d0e45539cec0f0a7a
Lawrence Helps <laurencehelps@hotmail.com>
2013-01-24 15:33:00 +0000

    CreatureClassAdded

---
+++ Adventures of Dr. Lazarus/Adventures of Dr. Lazarus/Creature/AttackTypes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CreatureGame.Creature
+{
+    public struct Attack
+    {
+        public string Name;
+        public float Damage;
+
+        public Attack(string Name, float Damage)
+        {
+            this.Name = Name;
+            this.Damage = Damage;
+        }
+    }
+
+    public struct AttackTypes
+    {
+        private List<Attack> Attacks;
+
+        public AttackTypes(Traits traits)
+        {
+            Attacks = new List<Attack>();
+
+            if (traits.Tail)
+            {
+                AvalibleAttacks.Add(new Attack("Whip", ((traits.Dexterity*traits.Agressiveness*traits.Strength)/3)/6));
+            }
+        }
+
+        public List<Attack> AvalibleAttacks
+        {
+            get { return Attacks; }
+        }
+    }
+}
---
+++ Adventures of Dr. Lazarus/Adventures of Dr. Lazarus/Creature/Creature.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CreatureGame.Creature
+{
+    class Creature
+    {
+        //private CreatureModel creatureModel;
+
+	    // Variables
+	    protected Traits Dominant;//A dominant allele always shows, even if the individual only has one copy of the allele.
+	    protected Traits Recessive;//A recessive allele only shows if the individual has two copies of the recessive allele.
+	    protected UInt16 BattlesLost;
+        private AttackTypes avTacks;
+
+	    //Constructors
+        public Creature(GraphicsDevice graphicsDevice)
+        {
+            Traits temp = new Traits();
+            temp.Pelvis = true;
+            temp.Spine = true;
+            temp.SpinalColumns = 4;
+            temp.Tail = true;
+            temp.TailColumns = 6;
+            Dominant = temp;
+            Recessive = temp;
+            CreateModel(graphicsDevice);
+            CreateAttacks();
+            BattlesLost = 0;
+        }
+
+        public Creature(ref Creature a, ref Creature b, GraphicsDevice graphicsDevice) // Could Gene Samples be included here, and passed in as a parameter, with a null default value?
+	    {                                                                              // If you don't want to implement them then I can do it, would just need another class and a few small modifications here.
+		    if(a.Recessive.Tail&b.Recessive.Tail)
+		    {
+			    Dominant.Tail=true;
+			    if (!(a.Dominant.Tail&b.Dominant.Tail))
+				    Recessive.Tail=false;
+			    else
+                {
+                    Recessive.Tail = new Random().Next(100) % 2 == 0;
+                }
+		    }
+		    else if (a.Dominant.Tail|b.Dominant.Tail)
+		    {
+			    if (a.BattlesLost<b.BattlesLost)
+			    {
+				    Dominant.Tail=true;
+				    Recessive.Tail=false;
+			    }
+			    else if (a.BattlesLost>b.BattlesLost)
+			    {
+				    Dominant.Tail=false;
+				    Recessive.Tail=true;
+			    }
+			    else
+			    {
+				    Dominant.Tail  = new Random().Next(100) % 2 == 0;
+				    Recessive.Tail = new Random().Next(100) % 2 == 0;
+			    }
+		    }
+		    else
+		    {
+                Dominant.Tail  = new Random().Next(100) % 2 == 0;
+                Recessive.Tail = new Random().Next(100) % 2 == 0;
+		    }
+
+		    if (b.Dominant.TailColumns!=0)
+			    Dominant.TailColumns=(ushort)(a.Dominant.TailColumns/b.Dominant.TailColumns);
+		    else
+			    Dominant.TailColumns=a.Dominant.TailColumns;
+
+		    CreateModel(graphicsDevice);
+            CreateAttacks();
+            BattlesLost = 0;
+	    }
+        
+	    // Methods
+        void CreateModel(GraphicsDevice graphicsDevice)
+        {
+        }
+
+        void CreateAttacks()
+        {
+            avTacks = new AttackTypes(this.Dominant);
+        }
+
+        public List<Attack> AvalibleAttacks
+        {
+            get { return avTacks.AvalibleAttacks; }
+        }
+
+        public void Draw(GraphicsDevice graphicsDevice)
+	    {
+            //creatureModel.Draw(Matrix.Identity);
+	    }
+    }
+}
