e35f3397ab1b38e23a10c0827388d02a6c68c593
Lawrence Helps <laurencehelps@hotmail.com>
2013-01-24 15:56:10 +0000

    Creature mod

--- Adventures of Dr. Lazarus/Adventures of Dr. Lazarus/Creature/Creature.cs
+++ Adventures of Dr. Lazarus/Adventures of Dr. Lazarus/Creature/Creature.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace CreatureGame.Creature
+namespace CreatureGame
 {
     class Creature
     {
