2e6018ee6a688d0773b23f8a800687aeb4af0ccb
Lawrence Helps <laurencehelps@hotmail.com>
2013-01-24 16:56:26 +0000

    Refresh

--- Code/Program/Source.cs
+++ Code/Program/Source.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +8,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using System.IO;
 
 namespace CreatureGame
 {
@@ -28,62 +28,6 @@ namespace CreatureGame
         {
             short temporary = 2345;
             test = new Creature(temporary);
-            base.Initialize();
-        }
-
-        protected override void LoadContent()
-        {
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-        }
-
-        protected override void UnloadContent()
-        {
-        }
-
-        protected override void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
-
-            base.Update(gameTime);
-        }
-
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            base.Draw(gameTime);
-        }
-    }
-}
-=======
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
-using System.IO;
-
-namespace CreatureGame
-{
-    public class Game1 : Microsoft.Xna.Framework.Game
-    {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
-
-        public Game1()
-        {
-            graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-        }
-
-        protected override void Initialize()
-        {
             base.Initialize();
         }
 
@@ -150,4 +94,3 @@ namespace CreatureGame
         }
     }
 }
->>>>>>> origin/Level
--- README.txt
+++ README.txt
@@ -1,2 +1,3 @@
-hello
-1
+Please do not break this repository
+
+use your own branch when working on code and do a merge when you want to share code with the rest of us
