6e4be40586fd77d1d6a6342f78faa9df471c5784
Henri Keeble <henrikeeble@btinternet.com>
2013-01-29 09:14:03 +0000

    Modified Item Class

--- Code/Program/Level/Item.cs
+++ Code/Program/Level/Item.cs
@@ -11,49 +11,44 @@ namespace VOiD
     {
         private enum itemName
         {
-            APPLE = 1,
-            GOLDEN_APPLE = 2,
-            SPRING_WATER = 3,
-            HONEY = 4,
-            CHILLI = 5
+            Apple = 1,
+            Golden_Apple = 2,
+            Spring_Water = 3,
+            Honey = 4,
+            Chilli = 5
         }
 
         private Texture2D _texture;
-        private string _name;
         private int _ID;
         private int _amount;
 
         public Item(int id, Microsoft.Xna.Framework.Content.ContentManager content)
         {
             _ID = id;
-            if (_ID == (int)itemName.APPLE)
-            {
-                _name = "Apple";
+            if (_ID == (int)itemName.Apple)
                 _texture = content.Load<Texture2D>("Sprites\\Apple");
-            }
-            if (_ID == (int)itemName.GOLDEN_APPLE)
-            {
-                _name = "Golden Apple";
+            if (_ID == (int)itemName.Golden_Apple)
                 _texture = content.Load<Texture2D>("Sprites\\GoldenApple");
-            }
-            if (_ID == (int)itemName.SPRING_WATER)
-            {
-                _name = "Spring Water";
+            if (_ID == (int)itemName.Spring_Water)
                 _texture = content.Load<Texture2D>("Sprites\\SpringWater");
-            }
-            if (_ID == (int)itemName.HONEY)
-            {
-                _name = "Honey";
+            if (_ID == (int)itemName.Honey)
                 _texture = content.Load<Texture2D>("Sprites\\Honey");
-            }
-            if (_ID == (int)itemName.CHILLI)
-            {
-                _name = "Chilli";
+            if (_ID == (int)itemName.Chilli)
                 _texture = content.Load<Texture2D>("Sprites\\Chilli");
+        }
+
+        public string Name
+        {
+            get
+            {
+                if (_ID != 0)
+                    return ((itemName)_ID).ToString().Replace('_', ' ');
+                else
+                    return "BLANK";
             }
         }
 
-        public string Name { get { return _name; } }
+
         public int ID { get { return _ID; } }
         public Texture2D Texture { get { return _texture; } }
         public int Amount { get { return _amount; } set { if (_amount + value < 99) _amount += value; } }
